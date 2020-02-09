e0393d2839b32abc1ff0978a1693d22e42d10efd
aliosmankasali <aliosmankasali@gmail.com>
2020-02-10 01:22:11 +0300

    Router ve Pug yapısı

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+//router require
+const userRouter = require('./routers/usersRouter');
+//view engine ayarları
 
+app.set('view engine','pug');
+
+//İndex Router
 app.get('/',(req,res,next)=>{
-    res.send('Merhaba Express');
+    res.render('index');
 });
+app.use('/admin', userRouter);
 
 app.listen(3000,()=>{
     console.log('sunucu Çalıştı');

cfb918a34d14e35c19b5c1ed8b1fa671b8db9d92
aliosmankasali <aliosmankasali@gmail.com>
2020-02-10 17:09:01 +0300

    Giriş formu hazırlandı

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 //router require
 const userRouter = require('./routers/usersRouter');
 //view engine ayarları
 
 app.set('view engine','pug');
+app.use(express.static(path.join(__dirname,'public')));
 
 //İndex Router
 app.get('/',(req,res,next)=>{
